bf5c905c3555200d1f56c573223ef795e1eee2cf
PercyGame <67379967+PercyGame@users.noreply.github.com>
2021-04-13 11:42:52 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function recupName(){
+	var userName = document.getElementById("input.name");
+	var userTryPessWord = document.getElementById("input.password");
+	alert("your name " + userName);
+	alert("your pass " + userTryPessWord);
+}
+
+function checkName(){
+	var pass = "True" /*Ceci est simplement destiné à passé la fonction sans la définir pour le moment*/
+}
+
+function checkPassWord(){
+	var pass = "True" /*Ceci est simplement destiné à passé la fonction sans la définir pour le moment*/
+}
